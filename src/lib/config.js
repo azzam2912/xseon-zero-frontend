d47c5415f1703f06f7bd357c82679f56a7412b9f
azzam2912 <azzam.gamers12@gmail.com>
2024-12-18 22:10:44 +0700

    config

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,15 +1,15 @@
-import { env } from '$env/dynamic/private';
+import { env } from '$env/dynamic/public';
 
-const apiBaseUrl = env.PRIVATE_API_DEV == 'local' ?  
-    env.PRIVATE_LOCAL_API_URL || 'http://localhost:3000/api' 
-       : env.PRIVATE_API_DEV == 'server' 
-          ? env.PRIVATE_SERVER_API_URL 
-          : env.PRIVATE_PROD_API_URL;
+const apiBaseUrl = env.PUBLIC_API_DEV == 'local' ?  
+    env.PUBLIC_LOCAL_API_URL || 'http://localhost:3000/api' 
+       : env.PUBLIC_API_DEV == 'server' 
+          ? env.PUBLIC_SERVER_API_URL 
+          : env.PUBLIC_PROD_API_URL;
 
 export const config = {
   apiUrl: apiBaseUrl,
-  getAuthUrl: (endpoint) => `${apiBaseUrl}${env.PRIVATE_BASE_AUTH}${endpoint}`,
-  getQuizUrl: (endpoint) => `${apiBaseUrl}${env.PRIVATE_BASE_QUIZ}${endpoint}`,
+  getAuthUrl: (endpoint) => `${apiBaseUrl}${env.PUBLIC_BASE_AUTH}${endpoint}`,
+  getQuizUrl: (endpoint) => `${apiBaseUrl}${env.PUBLIC_BASE_QUIZ}${endpoint}`,
 };
 
 export default config;
